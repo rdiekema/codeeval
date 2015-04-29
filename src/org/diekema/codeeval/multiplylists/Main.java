6ac8e49b38896c6a41327c9c7a96171c1aed7135
Richard Diekema <rdiekema@directvetmarketing.com>
2015-04-28 21:02:11 -0400

    completed multiplylists challenge.

---
+++ src/org/diekema/codeeval/multiplylists/Main.java
@@ -0,0 +1,30 @@
+package org.diekema.codeeval.multiplylists;
+
+import java.io.*;
+
+/**
+ * Created by rdiekema on 4/28/15.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 0) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(args[0]))));
+
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] set1 = line.split("\\|")[0].trim().split(" ");
+                String[] set2 = line.split("\\|")[1].trim().split(" ");
+
+                for(int i = 0; i < set1.length; i++){
+                    stringBuilder.append(Integer.valueOf(set1[i]) * Integer.valueOf(set2[i])).append(" ");
+                }
+
+                stringBuilder.append("\n");
+            }
+
+            System.out.println(stringBuilder.toString().trim());
+        }
+    }
+}
