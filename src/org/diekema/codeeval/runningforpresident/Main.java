b4e3a30c09c965c6c9db156e3a7cfba31007c84b
Richard Diekema <rdiekema@directvetmarketing.com>
2015-05-02 22:06:11 -0400

    Completed runningforpresidentchallenge

--- src/org/diekema/codeeval/runningforpresident/Main.java
+++ src/org/diekema/codeeval/runningforpresident/Main.java
@@ -10,7 +10,10 @@ public class Main {
     static int numberOfIssues = 0;
     static Map<String, Integer> issuesCostsMap = new HashMap<>();
     static Map<State, Map<String, Integer>> statesVotesIssuesMap = new HashMap<>();
-    static Integer ELECTORAL_MAJORITY = 10;
+    static Map<String, Integer> issueVoteCountMap = new HashMap<>();
+    static Integer ELECTORAL_MAJORITY = 9;
+
+    static List<List<String>> simulatedCampaigns = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         if (args.length > 0) {
@@ -25,6 +28,7 @@ public class Main {
 
             while (line.length() != 0) {
                 issuesCostsMap.put(line.substring(0, line.indexOf(':')), Integer.valueOf(line.substring(line.indexOf(':') + 2, line.length())));
+                issueVoteCountMap.put(line.substring(0, line.indexOf(':')), 0);
                 line = bufferedReader.readLine();
             }
 
@@ -38,8 +42,16 @@ public class Main {
                 line = bufferedReader.readLine();
                 state.votes = Integer.valueOf(line.substring(line.indexOf(':') + 2, line.length()).trim());
                 line = bufferedReader.readLine();
+
+                String issueName = "";
+                Integer voteCount = 0;
                 while (line != null && line.length() != 0) {
-                    state.issuesVotesMap.put(line.substring(0, line.indexOf(':')), Integer.valueOf(line.substring(line.indexOf(':') + 2, line.length())));
+                    issueName = line.substring(0, line.indexOf(':'));
+                    voteCount = Integer.valueOf(line.substring(line.indexOf(':') + 2, line.length()));
+                    state.issuesVotesMap.put(issueName, voteCount);
+                    if (issueVoteCountMap.containsKey(issueName)) {
+                        issueVoteCountMap.put(issueName, issueVoteCountMap.get(issueName) + voteCount);
+                    }
                     line = bufferedReader.readLine();
                 }
                 line = bufferedReader.readLine();
@@ -47,35 +59,138 @@ public class Main {
                 statesVotesIssuesMap.put(state, state.issuesVotesMap);
             }
 
-            // Now that we've processed all of the states and the votes
-            for (String issue : determineOptimalCampaign()) {
+            simulateCampaigns();
+            simulateCampaigns2();
+
+            List<String> optimalCampaign = determineOptimalCampaign();
+            Collections.sort(optimalCampaign);
+            for (String issue : optimalCampaign) {
                 System.out.println(issue);
             }
+
         }
     }
 
     static List<String> determineOptimalCampaign() {
-        List<String> campaignIssues = new ArrayList<>();
-        campaignIssues.addAll(issuesCostsMap.keySet());
+        Integer cheapestCampaignValue = Integer.MAX_VALUE;
+        Integer currentCampaignTotal = 0;
+        Integer fewestIssueCount = Integer.MAX_VALUE;
 
+        List<String> fewestIssues = new ArrayList<>();
+        for (List<String> simulatedCampaign : simulatedCampaigns) {
+            if (simulatedCampaign.size() < fewestIssueCount) {
+                fewestIssueCount = simulatedCampaign.size();
+                fewestIssues = simulatedCampaign;
+            }
+        }
+
+        List<String> cheapestCampaign = new ArrayList<>();
+        // Calculate cost for all campaigns
+        for (List<String> simulatedCampaign : simulatedCampaigns) {
+
+            // Calculate campaign cost
+            for (String issue : simulatedCampaign) {
+                currentCampaignTotal += issuesCostsMap.get(issue);
+            }
+
+            if (currentCampaignTotal < cheapestCampaignValue) {
+                cheapestCampaign = simulatedCampaign;
+            }
+        }
+
+        return cheapestCampaign.size() > fewestIssues.size() ? fewestIssues : cheapestCampaign;
+    }
+
+    static void simulateCampaigns() {
         List<State> orderedStates = new ArrayList<>();
         orderedStates.addAll(statesVotesIssuesMap.keySet());
         Collections.sort(orderedStates);
 
-        List<String> hypotheticalCampaign = new ArrayList<>();
-        Integer campaignTotal = 0;
-        int i = 0;
-        while (i < campaignIssues.size() && campaignTotal < ELECTORAL_MAJORITY) {
-            hypotheticalCampaign.add(campaignIssues.get(i));
-            for (State state : orderedStates) {
-                if (winState(state, hypotheticalCampaign)) {
-                    campaignTotal += state.votes;
-                }
+        LinkedList<Map.Entry<String, Integer>> linkedList = new LinkedList(issueVoteCountMap.entrySet());
+
+        Collections.sort(linkedList, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return ((Comparable) ((Map.Entry) (o2)).getValue()).compareTo(((Map.Entry) (o1)).getValue());
             }
-            i++;
+        });
+
+        Iterator<Map.Entry<String, Integer>> linkedListIterator = linkedList.iterator();
+        while (linkedListIterator.hasNext()) {
+            List<String> hypotheticalCampaign = new ArrayList<>();
+            Integer campaignTotal = 0;
+            int i = 0;
+            while (linkedListIterator.hasNext() && campaignTotal < ELECTORAL_MAJORITY) {
+                hypotheticalCampaign.add(linkedListIterator.next().getKey());
+                for (State state : orderedStates) {
+                    if (winState(state, hypotheticalCampaign)) {
+                        campaignTotal += state.votes;
+                        if (campaignTotal >= ELECTORAL_MAJORITY) {
+                            break;
+                        }
+                    }
+                    if (campaignTotal >= ELECTORAL_MAJORITY) {
+                        break;
+                    }
+                }
+                i++;
+            }
+
+            simulatedCampaigns.add(hypotheticalCampaign);
+        }
+    }
+
+    static void simulateCampaigns2() {
+        List<State> orderedStates = new ArrayList<>();
+        orderedStates.addAll(statesVotesIssuesMap.keySet());
+        Collections.sort(orderedStates);
+        Collections.reverse(orderedStates);
+
+        LinkedList<Map.Entry<String, Integer>> linkedList = new LinkedList(issueVoteCountMap.entrySet());
+
+        Collections.sort(linkedList, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return ((Comparable) ((Map.Entry) (o2)).getValue()).compareTo(((Map.Entry) (o1)).getValue());
+            }
+        });
+
+        /*
+         In ordered states there is a list of states ordered by the total number of votes available.
+         In the original method the simulation iterated over all of the state and added a new issue
+         to the hypothetical campaign and then check to see if that issue won enough votes to push
+         the election over the ELECTORAL_MAJORITY threshold. in simulateCampaigns2 the goal will be
+         to start in the reverse order with the states to build a second set of potential campaigns
+         starting with the states having the fewest numbers of votes, but still ordering the issues
+         by overall available vote count. This should give a few new hypothetical campaigns that
+         provide a way to get to 270 votes without the election being dominated by the larger state
+         priorities.
+          */
+
+        Iterator<Map.Entry<String, Integer>> linkedListIterator = linkedList.iterator();
+        while (linkedListIterator.hasNext()) {
+            List<String> hypotheticalCampaign = new ArrayList<>();
+            Integer campaignTotal = 0;
+            int i = 0;
+            while (linkedListIterator.hasNext() && campaignTotal < ELECTORAL_MAJORITY) {
+                hypotheticalCampaign.add(linkedListIterator.next().getKey());
+                for (State state : orderedStates) {
+                    if (winState(state, hypotheticalCampaign)) {
+                        campaignTotal += state.votes;
+                        if (campaignTotal >= ELECTORAL_MAJORITY) {
+                            break;
+                        }
+                    }
+                    if (campaignTotal >= ELECTORAL_MAJORITY) {
+                        break;
+                    }
+                }
+                i++;
+            }
+
+            simulatedCampaigns.add(hypotheticalCampaign);
         }
 
-        return hypotheticalCampaign;
     }
 
     static boolean winState(State state, List<String> issues) {
@@ -85,7 +200,7 @@ public class Main {
         for (String issue : issues) {
             if (state.issuesVotesMap.containsKey(issue)) {
                 votesWonByIssues += state.issuesVotesMap.get(issue);
-                if (votesWonByIssues / electoralVotes > .51d) {
+                if (new Double(votesWonByIssues / electoralVotes).compareTo(.51d) > 0) {
                     return true;
                 }
             }
